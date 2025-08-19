07a4710da7cfb332d204b8163d85a9da38b91039
miguel.jaeger <miguel.jaeger@gmail.com>
2025-08-18 23:19:30 -0500

    add list off students

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+Route::get('/students', function () {
+    return "List off students";
+});
+
