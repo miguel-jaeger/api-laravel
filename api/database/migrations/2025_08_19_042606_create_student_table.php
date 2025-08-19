27ebc7967655f011ef61b1ba0fa24155c4d9c769
miguel.jaeger <miguel.jaeger@gmail.com>
2025-08-18 23:28:49 -0500

    modify student fields migration

--- api/database/migrations/2025_08_19_042606_create_student_table.php
+++ api/database/migrations/2025_08_19_042606_create_student_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('student', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('language');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
